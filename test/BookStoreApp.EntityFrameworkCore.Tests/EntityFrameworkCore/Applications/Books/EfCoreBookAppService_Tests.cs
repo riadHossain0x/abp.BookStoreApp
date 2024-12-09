027b06c766c80c0e58411f1420f0adfb89180896
riadHossain <riadoffiicial.me@gmail.com>
2024-12-09 12:13:20 +0600

    BookAppService Tests

---
+++ test/BookStoreApp.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,30 @@
+﻿using BookStoreApp.Books;
+using Shouldly;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace BookStoreApp.EntityFrameworkCore.Applications.Books;
+
+public class EfCoreBookAppService_Tests : BookAppService_Tests<BookStoreAppEntityFrameworkCoreModule>
+{
+    [Fact]
+    public async Task Should_Not_Create_A_Book_Withoud_Name()
+    {
+        var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+        {
+            await _bookAppService.CreateAsync(
+                new CreateUpdateBookDto
+                {
+                    Name = "",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                });
+        });
+
+        exception.ValidationErrors.ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+    }
+}
